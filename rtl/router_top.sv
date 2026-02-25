99817f8f0afa8dd8c34b20f1ed1637009a38a6a1
10SAhmed <saad_inbox@yahoo.com>
2026-02-25 23:37:11 +0500

    updated fifo read_en and write_en logic in router top and wrote a FIXED arbiter logic

--- rtl/router_top.sv
+++ rtl/router_top.sv
@@ -5,23 +5,29 @@ module router_top #(
   parameter PRIORITY_MODE = FIXED
 ) (
   //  General Signals
-  input  logic                  clk_i              , // Clock
-  input  logic                  rst_i              , // Synchronous reset active high
+  input  logic                                  clk_i  , // Clock
+  input  logic                                  rst_i  , // Synchronous reset active high
   // Ingress Interface
-  input  logic                  valid_i[NUM_INPUTS], // per port data validation signal
-  input  logic [DATA_WIDTH-1:0] data_i [NUM_INPUTS], // per port data stream
-  input  logic                  start_i[NUM_INPUTS], // per port start of packet indicator
-  output logic                  ready_o[NUM_INPUTS], // per port DUT is ready signal
+  input  logic [NUM_INPUTS-1:0]                 valid_i, // per port data validation signal
+  input  logic [NUM_INPUTS-1:0][DATA_WIDTH-1:0] data_i , // per port data stream
+  input  logic [NUM_INPUTS-1:0]                 start_i, // per port start of packet indicator
+  output logic [NUM_INPUTS-1:0]                 ready_o, // per port DUT is ready signal
   // Egress Interface
-  output logic                  valid_o[NUM_INPUTS], // per port data validation signal
-  output logic [DATA_WIDTH-1:0] data_o [NUM_INPUTS], // per port data stream
-  output logic                  sop_o  [NUM_INPUTS], // per port start of packet indicator
-  input  logic                  ready_i[NUM_INPUTS]  // per port Slave is ready signal
+  output logic [NUM_INPUTS-1:0]                 valid_o, // per port data validation signal
+  output logic [NUM_INPUTS-1:0][DATA_WIDTH-1:0] data_o , // per port data stream
+  output logic [NUM_INPUTS-1:0]                 sop_o  , // per port start of packet indicator
+  input  logic                                  ready_i  // per port Slave is ready signal
 );
 
   // interconnect
-  bit full, empty;
-  bit wen, ren;
+  bit [NUM_INPUTS-1:0] wen  ;
+  bit [NUM_INPUTS-1:0] ren  ;
+  bit [NUM_INPUTS-1:0] full ;
+  bit [NUM_INPUTS-1:0] empty;
+  bit [NUM_INPUTS-1:0] grant;
+
+  assign wen = valid_i & ready_o;
+  assign ren = {NUM_INPUTS{ready_i}} & grant;
 
   // per port FIFO
   genvar i;
@@ -32,13 +38,17 @@ module router_top #(
         .clk_i  (clk_i    ),
         .rst_i  (rst_i    ),
         .data_i (data_i[i]),
-        .wen_i  (wen      ),
-        .ren_i  (ren      ),
-        .full_o (full     ),
-        .empty_o(empty    ),
+        .wen_i  (wen[i]   ),
+        .ren_i  (ren[i]   ),
+        .full_o (full[i]  ),
+        .empty_o(empty[i] ),
         .data_o (data_o[i])
       );
     end
   endgenerate
 
+  arbiter #(.NUM_INPUTS(NUM_INPUTS), .PRIORITY_MODE(PRIORITY_MODE)) arbiter_inst (
+    .fifo_empty(empty),
+    .grant     (grant)
+  );
 endmodule : router_top
